3f150e8cb11588722cff8f35ce8f7c52c868fe0a
aletheist <aletheist@users.noreply.github.com>
2016-08-26 16:08:23 -0400

    Name python file correctly.

--- anthologion.py
+++ anthologion.py
@@ -4,10 +4,16 @@ import pypandoc as pandoc
 from horologion.generate_hour import generate_service
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/services")
 def midnight_service():
     markdown = generate_service("oc", "eastern", "en-us", "hmbm", "midnight")
     return pandoc.convert_text(markdown, 'html5', format='md', extra_args=['-s'])
+
+@app.route("/")
+def homepage():
+    with open("index.html") as blat:
+        index = blat.read()
+    return index
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("80"), debug=False)
